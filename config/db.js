e85667de7c84420308685884dd416daa7f104b44
SunitraS <sunitraprivate@gmail.com>
2025-01-18 08:16:15 +0700

    Update Mongo

--- config/db.js
+++ config/db.js
@@ -2,12 +2,17 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/Blog');
+    // แก้ไข MongoDB URI ให้เป็นของ MongoDB Atlas
+    await mongoose.connect('mongodb+srv://root:root1234@cluster0.rmbya.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
     console.log('MongoDB connected');
   } catch (err) {
     console.error('Error connecting to MongoDB:', err);
     process.exit(1);
   }
 };
+
 
 module.exports = connectDB;
